3b244fe16b86dc15b1519136d575580267cf25b6
Justin Sturm <jsturm73@yahoo.com>
2015-07-20 03:30:12 -0700

    Fixed dimmer switch, added scale animation.

--- js/main.js
+++ js/main.js
@@ -22,7 +22,6 @@ $('.playList li .hideBoxHide .download').on('click', function () {
 });
 
 
-
 $('.download a').attr( "href", function() {
     return "http://invisibleglue.com/audio/" + this.title + ".mp3";
 });
@@ -30,17 +29,155 @@ $('.download a').attr( "href", function() {
 
 $("video").prop('muted', true);
 
-$("#videoMute").click( function (){
+$("#videoMuteIcon").click( function (){
+    $('#videoMuteIcon').toggleClass('fa-volume-up');
+
     if( $("video").prop('muted') ) {
         $("video").prop('muted', false);
     } else {
         $("video").prop('muted', true);
     }
 });
-//$('.playAnim').load('/BootSnippetTest/lyrics.html #loaded');
-//$('#loaded').css({'opacity': 1});
 
+//return a DOM object
+var video = document.getElementById('wayOutSplash'); //or
+var video = $('#wayOutSplash').get(0); //or
+var video = $('#wayOutSplash')[0];
 
-//$('footer .amplitude-play-pause').on('click', function () {
-//    $('#playIcon').toggleClass('fa-pause');
-//});
+//return a jQuery object
+var video = $('#wayOutSplash');
+
+    video.on('loadedmetadata', function() {
+        $('.duration').text(video[0].duration);
+    });
+    video.on('timeupdate', function() {
+        $('.current').text(video[0].currentTime);
+    });
+
+    $("#videoPlayPause").click(function () {
+        $('#videoPpIcon').toggleClass('fa-pause', 'fa-play'); //Adds 'a', removes 'b' and vice versa
+        var video = $('#wayOutSplash')[0];
+            if(video.paused) {
+                video.play();
+            } else {
+                video.pause();
+            }
+        return false;
+    });
+
+    //get HTML5 video time duration
+    video.on('loadedmetadata', function() {
+        $('.duration').text(video[0].duration);
+    });
+
+    //update HTML5 video current play time
+    video.on('timeupdate', function() {
+        var currentPos = video[0].currentTime; //Get currenttime
+        var maxduration = video[0].duration; //Get video duration
+        var percentage = 100 * currentPos / maxduration; //in %
+        $('.timeBar').css('width', percentage+'%');
+    });
+    var timeDrag = false;   /* Drag status */
+    $('.progressBar').mousedown(function(e) {
+        timeDrag = true;
+        updatebar(e.pageX);
+    });
+    $(document).mouseup(function(e) {
+        if(timeDrag) {
+            timeDrag = false;
+            updatebar(e.pageX);
+        }
+    });
+    $(document).mousemove(function(e) {
+        if(timeDrag) {
+            updatebar(e.pageX);
+        }
+    });
+
+    //update Progress Bar control
+    var updatebar = function(x) {
+        var progress = $('.progressBar');
+        var maxduration = video[0].duration; //Video duraiton
+        var position = x - progress.offset().left; //Click pos
+        var percentage = 100 * position / progress.width();
+
+        //Check within range
+        if(percentage > 100) {
+            percentage = 100;
+        }
+        if(percentage < 0) {
+            percentage = 0;
+        }
+
+        //Update progress bar and video currenttime
+        $('.timeBar').css('width', percentage+'%');
+        video[0].currentTime = maxduration * percentage / 100;
+    };
+
+    //loop to get HTML5 video buffered data
+    var startBuffer = function() {
+        var maxduration = video[0].duration;
+        var currentBuffer = video[0].buffered.end(0);
+        var percentage = 100 * currentBuffer / maxduration;
+        $('.bufferBar').css('width', percentage+'%');
+
+        if(currentBuffer < maxduration) {
+            setTimeout(startBuffer, 500);
+        }
+    };
+    setTimeout(startBuffer, 500);
+
+$('#fullScreen').on('click', function() {
+    //For Webkit
+    video[0].webkitRequestFullscreen();
+
+    //For Firefox
+    video[0].mozRequestFullScreen();
+
+    return false;
+});
+
+$('#btnLight').click(function() {
+    if($(this).hasClass('on')) {
+        $(this).removeClass('on');
+        $('html').prepend('<div class="overlay"></div>');
+        $('.overlay').animate({
+            'opacity': 0.8
+        }, 1200);
+        $('.videoCenter').css({
+            'z-index':1000
+        });
+        $('.buttonCenter').css({
+            'z-index': 1001
+        });
+        $('#btnLightIcon').removeClass('fa-inverse', 1000).addClass('fa-reverse', 1000);
+        $('video').animate({
+           'width': '60%',
+            'height': '60%'
+        }, 1000);
+        $('.buttonCenter').animate({
+            'width': '60%',
+            //'height': '60%',
+            'left': '50%',
+            'margin-left': '-405px',
+            'top': '-50px'
+        }, 1000);
+
+    } else {
+        $(this).addClass('on');
+        $('.overlay').animate({
+            'opacity': 0
+        }, 1200);
+        $('#btnLightIcon').removeClass('fa-reverse', 1000).addClass('fa-inverse', 1000);
+        $('video').animate({
+            'width': '640px',
+            'height': '360px'
+        }, 1000);
+
+        $('.buttonCenter').animate({
+            'width': '640px',
+            'height': '48px'
+        }, 1000);
+    }
+    return false;
+});
